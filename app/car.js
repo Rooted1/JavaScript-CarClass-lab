ec2c4b166b8623bad635d59e8aa09102446d1642
Rooted1 <ruthobey@gmail.com>
2016-11-28 16:23:52 +0100

    compare different class cases

--- app/car.js
+++ app/car.js
@@ -1,22 +1,31 @@
-module.exports = {
-  Car  : function (type, model, name) {
+var myObj = {
+  Car  : function (name, model, type) {
   this.type = type;
+  this.model = model;
+  this.name = name;
+  this.numOfDoors = 4;
+  this.numOfWheels = 4;
   
-  if (model !== undefined){
-    this.model = model;
-  } else {
-    this.model = "GM";
-  }
-  
-  if(name !== undefined){
-    this.name = name;
-  }
-  else{
-   this.name = "General";
+  if(name === undefined || model === undefined){
+    this.name = "General";
+    this.model ="GM";
   }
 
-  // this.info = function () {
-  //   return "The type of this car is " + this.type + " and it's model is " + this.model + " and name is " + this.name;
-  // };
+  if(this.name === "Porshe" || this.name === "Koenigsegg"){
+    this.numOfDoors = 2;
+  }
 }
-}
+};
+module.exports = myObj;
+
+
+// it("The car shoud have four (4) doors except its a Porshe or Koenigsegg", function() {
+//       var opel  = new Car('Opel', 'Omega 3');
+//       expect(opel.numOfDoors).to.equal(4);
+
+//       var porshe = new Car('Porshe', '911 Turbo');
+//       expect(porshe.numOfDoors).to.equal(2);
+//       porshe.drive(5);
+//       expect(porshe.speed).to.equal('250 km/h');
+//       expect((function(){return new Car('Koenigsegg', 'Agera R');}()).numOfDoors).toBe(2);
+//     });
